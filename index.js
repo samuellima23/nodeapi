5dcc518491a6373e274ccedc07720f25e646f7c7
Samuel Lima <samuelshigen@gmail.com>
2020-01-04 20:18:51 -0300

    make it better

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+//iniciando o app
+const app = express();
+app.use(express.json());
+app.use(cors());
+//iniciando o db
+mongoose.connect('mongodb://localhost:27017/nodeapi', 
+{useNewUrlParser:true,useUnifiedTopology:true}
+)
+
+require('./src/models/Product');
+
+//rotas
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
